6f4de8c33f90a8fc603909c46fe5f0b82cfaf286
Sam Oldenburg <sam.oldenburg@gmail.com>
2026-03-16 19:11:52 -0500

    fix timing window disabling

--- Scripts/SL-PlayerOptions.lua
+++ Scripts/SL-PlayerOptions.lua
@@ -836,7 +836,9 @@ local Overrides = {
 		Values = function()
 			return {
 				{true,true,true,true,true},
+				{true,true,true,true,false},
 				{true,true,true,false,false},
+				{false,false,true,true,true},
 			}
 		end,
 		Choices = function()
